77d773d17f51961fc6158550510385115c059693
Nguyen Chon Thang <tthangchon@gmail.com>
2022-08-17 16:45:40 +0700

    update

--- src/main/java/com/example/controller/ProRestController.java
+++ src/main/java/com/example/controller/ProRestController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.Project;
 import com.example.service.ProjectService;
+import com.example.utils.Response;
 
 @RestController
 @RequestMapping("/api/project")
@@ -22,5 +25,12 @@ public class ProRestController {
 	public List<Project> getProjects() {
 		List<Project> projects = projectService.getProjects();
 		return projects;
+	}
+	
+	@CrossOrigin
+	@PostMapping("/add")
+	public Response addProject(@RequestBody Project project) {
+		projectService.addProject(project);
+		return new Response(0, "");
 	}
 }
